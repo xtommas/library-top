ea21d9155f81865239807c3071bd032c0fca5051
tomas <tomascanepa1397@gmail.com>
2022-11-02 12:50:45 -0300

    added modal window to UI

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let mylibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -5,3 +7,22 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector("#newButton");
+const closeModalBtn = document.querySelector(".closeModal");
+
+const openModal = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+};  
+
+openModalBtn.addEventListener("click", openModal);
+
+const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+}
+
+overlay.addEventListener("click", closeModal);
+closeModalBtn.addEventListener("click", closeModal);
